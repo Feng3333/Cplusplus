b6cf2b23e19312a7c6e9895147265d5afec064eb
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2023-05-22 15:03:54 +0800

    Update test.cpp

--- python调用C++/test/test.cpp
+++ python调用C++/test/test.cpp
@@ -2,7 +2,7 @@
 
 extern "C" {
     char* test(char* n) {
-        std::cout << "The C++ input char is " << n;
+        std::cout << "The C++ input char is " << n << std::endl;
         return n;
     }
 }

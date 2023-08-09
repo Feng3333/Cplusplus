c19c3d28da303030f39ddc5457785bc6e1d75881
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2023-08-09 11:21:41 +0800

    Create table_driver_test.cpp

---
+++ C++表驱动模式/table_driver_test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+
+class FunMap {
+public:
+
+    int func1(int a, int b)
+    {
+        return a - b;
+    }
+
+    int func2(int a, int b) 
+    {
+        return a + b;
+    }
+
+    typedef int(FunMap::*FUN)(int a, int b);
+    std::map<std::string, FUN> map = {
+        {"+", &FunMap::func1},
+        {"-", &FunMap::func2},
+    };
+};
+
+int main() {
+    FunMap *m = new FunMap();
+    int a = 10, b = 5;
+    std::string str("-");
+    FunMap::FUN f = m->map[str];
+    int ans = (m->*f)(a, b);
+    std::cout << ans;
+    return 0;
+}
